966febd5ae70b3de4cd57d41eb5f2b436ad168e4
akbaroke <akbarmuhammad833@gmail.com>
2022-10-29 20:48:29 +0700

    update lap recorder

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-let jam = 0;
 let menit = 0;
 let detik = 0;
+let mDetik = 0;
 
 let pause = false;
 setInterval(()=>{
   if(pause){
     setTimeout(()=>{
+      if(mDetik < 10){
+        $('#mDetik').html("0"+mDetik);
+      }else{
+        $('#mDetik').html(mDetik);
+      }
       if(detik < 10){
         $('#detik').html("0"+detik);
       }else{
@@ -16,32 +21,31 @@ setInterval(()=>{
       }else{
         $('#menit').html(menit);
       }
-      if(jam < 10){
-        $('#jam').html("0"+jam);
-      }else{
-        $('#jam').html(jam);
-      }
-    },0);
-    detik++;
+    },);
+    mDetik++;
+    if(mDetik == 99){
+      mDetik = 0;
+      detik++;
+    }
     if(detik == 60){
       detik = 0;
       menit++;
     }
     if(menit == 60){
       menit = 0;
-      jam++;
     }
   }
-}, 1000);
+}, 10);
 
 
 // tombol pause
 $('#btn-pause').click(function (e) { 
   e.preventDefault();
   pause = false;
+  $('#btn-lap').css('display','none');
   $('#btn-play').css('display','block');
   $('#btn-pause').css('display','none');
-  if(detik > 0 || menit > 0 || jam > 0){
+  if(mDetik > 0 || detik > 0 || menit > 0){
     $('#btn-reset').css('display','block');
     $('#putar').css('animation-play-state','paused');
   }
@@ -52,6 +56,7 @@ $('#btn-play').click(function (e) {
   e.preventDefault();
   $('#btn-play').css('display','none');
   $('#btn-pause').css('display','block');
+  $('#btn-lap').css('display','block');
   $('#btn-reset').css('display','none');
   $('#putar').css('animation-play-state','running');
   pause = true;
@@ -61,12 +66,30 @@ $('#btn-play').click(function (e) {
 $('#btn-reset').click(function (e) { 
   e.preventDefault();
   pause = false;
-  jam = 0;
   menit = 0;
   detik = 0;
+  mDetik = 0;
+  $('#mDetik').html("00");
   $('#detik').html("00");
   $('#menit').html("00");
-  $('#jam').html("00");
   $('#btn-reset').css('display','none');
   location.reload(true);
 });
+
+
+let nomer = 1;
+// lap record
+$('#btn-lap').click((e)=>{
+  e.preventDefault();
+  $('#lap-record').append(`
+  <div>
+  <p>`+nomer+`</p>
+  <h3>`+$('#menit').text()+`:`+$('#detik').text()+`:`+$('#mDetik').text()+`</h3>
+  </div>
+  `)
+  if(nomer === 1){
+    $('.lingkaran-besar').css('bottom','-60px');
+    $('#putar').css('top','-245px');
+  }
+  nomer++;
+})
